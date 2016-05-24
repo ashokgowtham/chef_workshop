a20353940422d52bdfbbc9befbdd20e64a932a8c
Ashok Gowtham M <ashokgowtham123@gmail.com>
2016-05-24 15:09:18 +0530

    Ashok | add environment and role

--- install_chef_server.sh
+++ install_chef_server.sh
@@ -2,5 +2,6 @@
 
 wget https://packages.chef.io/stable/el/5/chef-server-core-12.6.0-1.el5.x86_64.rpm
 rpm -Uvh chef-server-core-12.6.0-1.el5.x86_64.rpm
+sudo chef-server-ctl reconfigure
 sudo chef-server-ctl install chef-manage
 sudo chef-server-ctl reconfigure
