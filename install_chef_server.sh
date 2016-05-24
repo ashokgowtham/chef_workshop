f24e18c0c53f4c542c117885774056d75be930d4
Ashok Gowtham M <ashokgowtham123@gmail.com>
2016-05-24 16:09:28 +0530

    Ashok | update install script

--- install_chef_server.sh
+++ install_chef_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
+sudo yum install -y wget
 wget https://packages.chef.io/stable/el/5/chef-server-core-12.6.0-1.el5.x86_64.rpm
-rpm -Uvh chef-server-core-12.6.0-1.el5.x86_64.rpm
+sudo rpm -Uvh chef-server-core-12.6.0-1.el5.x86_64.rpm
 sudo chef-server-ctl reconfigure
 sudo chef-server-ctl install chef-manage
 sudo chef-server-ctl reconfigure
